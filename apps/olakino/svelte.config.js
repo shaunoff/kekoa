7cdc26044d9fc0efc85e3e75c98aabec8d3f814c
Shaun Hutch <shaun@flipsidecrypto.com>
2023-08-12 14:46:06 -0400

    minify css

--- apps/olakino/svelte.config.js
+++ apps/olakino/svelte.config.js
@@ -3,8 +3,6 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
